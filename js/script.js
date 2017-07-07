c8557b11abecd63c03e64a01a15b96f3e4c1ca60
HudsonTXT <livecheg@ya.ru>
2017-07-08 01:04:34 +0500

    first day of work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-      var tag = document.createElement('script');
+/*anim*/
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+/*youtube*/
+var tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -19,3 +29,20 @@
           // Mute!
           player.mute();
       }
+/*text first screen*/
+      sectionText = ['танцуй', 'учись', 'развивайся', 'слушай', 'будь лучшим', 'смотри'];
+      var sectionId = 0;
+
+      setInterval(function () {
+          $('.head span').fadeOut(300, function () {
+              if (sectionId == sectionText.length-1) {
+                  sectionId = 0;
+              } else {
+                  sectionId++;
+              }
+              $('.head span').text(sectionText[sectionId]);
+              $('.head span').fadeIn(300);
+          });
+
+
+      }, 3000);
