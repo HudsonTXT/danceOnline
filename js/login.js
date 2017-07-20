1574b5b96d9b84103f5df3e3fae2142650282a86
hudsontxt <livecheg@ya.ru>
2017-07-20 15:32:56 +0500

    some fixes with login

--- js/login.js
+++ js/login.js
@@ -1,40 +1,48 @@
 var auth;
+var authorized = false;
 $(function () {
     VK.init({
         apiId: 4004433
     });
     $('.button').click(function () {
-        VK.Auth.login(function (a) {
-            if (a.session) {
-                auth = a.session;
-                $.post('http://fandance.ru/music/events.php?do=login', {
-                        id: auth.mid,
-                        name: auth.user.first_name,
-                        last: auth.user.last_name,
-                        href: auth.user.href
-                    },
-                    function (data) {
-                        if (data.length > 0) {
-                            alert(data[0]);
-                        } else {
-                            check_login();
-                        }
-                    }, "json");
-                //alert('Добро Пожаловать, ' + auth.user.first_name);
+        if (authorized == false) {
+            VK.Auth.login(function (a) {
+                if (a.session) {
+                    auth = a.session;
+                    $.post('//fandance.ru/music/events.php?do=login', {
+                            id: auth.mid,
+                            name: auth.user.first_name,
+                            last: auth.user.last_name,
+                            href: auth.user.href
+                        },
+                        function (data) {
+                            if (data.length > 0) {
+                                alert(data[0]);
+                            } else {
+                                check_login();
+                            }
+                        }, "json");
+                    //alert('Добро Пожаловать, ' + auth.user.first_name);
+                    location.href = 'lk/';
 
-            } else {
-                alert('К сожалению, вы не смогли авторизоваться.<br/>Пожалуйста, повторите попытку ещё раз.');
-            }
-            //console.log(auth);
+                } else {
+                    alert('К сожалению, вы не смогли авторизоваться. Пожалуйста, повторите попытку ещё раз.');
+                }
+                //console.log(auth);
 
-        });
+            });
+        } else {
+            location.href = 'lk/';
+        }
+
+
 
 
     });
     check_login();
 
     $('#logout').click(function () {
-        $.getJSON('http://fandance.ru/music/events.php?do=logout', function (json) {
+        $.getJSON('//fandance.ru/music/events.php?do=logout', function (json) {
             alert(json.mes);
             location.reload();
         });
@@ -42,22 +50,12 @@ $(function () {
 });
 
 function check_login() {
-    $.getJSON('http://fandance.ru/music/events.php?do=check_login', function (json) {
+    $.getJSON('//fandance.ru/music/events.php?do=check_login', function (json) {
         if (json.loginned) {
-            //Loginned user
-            VK.Api.call('users.get', {
-                user_ids: json.user.mid,
-                fields: 'photo_50'
-            }, function (r) {
-                user = r.response[0];
-
-                $('.user-image img').attr('src', user.photo_50);
-                $('span.username').text(user.first_name + ' ' + user.last_name);
-                $('span.coins').text(user.coins);
-            })
-            location.href = 'lk/';
+            authorized = true;
+            $('.button').text('Продолжить');
         } else {
-            //Not lodinned
+            authorized = false;
         }
     });
 }
